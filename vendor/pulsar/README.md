1c2066e4f1632a2e42471f86ed86a4cadbe65565
confused-Techie <dev@lhbasics.com>
2026-04-03 13:48:32 -0700

    Pull in vendor `pulsar`

---
+++ vendor/pulsar/README.md
@@ -0,0 +1,13 @@
+# vendor/pulsar
+
+Files taken from [`pulsar-edit/pulsar`](https://github.com/pulsar-edit/pulsar).
+
+### jasmine-singleton
+
+Pulled from `pulsar-edit/pulsar/spec/helpers/jasmine-singleton.js`.
+
+### jasmine
+
+Pulled from `pulsar-edit/pulsar/vendor/jasmine.js`.
+
+Which seemingly originates from `mhevery/jasmine-node/lib/jasmine-node/jasmine-1.3.1.js`.
---
+++ vendor/pulsar/jasmine-singleton.js
@@ -0,0 +1,16 @@
+let jasmine;
+
+jasmineVendor = require("./jasmine.js");
+for (let key in jasmineVendor) { window[key] = jasmineVendor[key]; }
+
+jasmine = jasmineVendor.jasmine;
+
+require("../jasmine-json");
+
+if ( !jasmine.TerminalReporter ) {
+  const { jasmineNode} = require("../jasmine-node/reporter.js");
+
+  jasmine.TerminalReporter = jasmineNode.TerminalReporter;
+}
+
+module.exports = jasmine;
