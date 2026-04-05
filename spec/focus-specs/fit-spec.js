3a1aec2512238d2d39a5b39d51b58f13f1213bd5
confused-Techie <dev@lhbasics.com>
2026-04-05 13:21:47 -0700

    Remove focus specs since they focus the entire spec dir

--- spec/focus-specs/fit-spec.js
+++
@@ -1,8 +0,0 @@
-describe("only runs the 'fit' spec", () => {
-  it("doesn't run this 'it'", () => {
-    expect(true).toBe(false);
-  });
-  fit("only runs this 'fit' spec", () => {
-    expect(true).toBe(true);
-  });
-});
